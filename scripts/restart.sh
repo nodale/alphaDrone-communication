6a49f2a4fd91a5fb5a2af2b82c5280897b829237
joey <koentarij@gmail.com>
2026-04-02 17:27:03 +0200

    added another background process

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,4 +1,5 @@
 sudo systemctl restart vicon_data_collector.service
+sudo systemctl restart triangulator.service
 sudo systemctl restart setpoint_data_collector.service
 sudo systemctl restart estimation_data_collector.service
 sudo systemctl restart viser.service
